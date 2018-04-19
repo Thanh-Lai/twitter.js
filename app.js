e454a3429412db14d13ce58c96d14fcfef7794c8
Thanh-Lai <tlai1122@gmail.com>
2018-04-19 11:08:04 -0400

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 
+app.use((req, res, next)=> {
+    let route = req.path
+    let verb = req.method
+    console.log(verb, route)
+    console.log(res.send(statusCode))
+    next()
+})
 app.get('/',(req, res, next)=>  res.send('Welcome!'))
-
+app.get('/news',(req, res, next)=>  res.send('News!'))
 
 
 
