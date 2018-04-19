86047f5912d303957cd3bd95f0076703f7a4edfc
Thanh-Lai <tlai1122@gmail.com>
2018-04-19 15:30:07 -0400

    fourth commit

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,15 @@ const tweetBank = require('../tweetBank');
 
 router.get('/', (req, res) => {
   let tweets = tweetBank.list();
-
   res.render('index', { tweets });
+})
+
+router.get('/users/:name', function(req, res){
+  let name = req.params.name;
+  let list = tweetBank.find({name: name});
+  res.render('index', {name})
+  console.log(list, name)
+
 })
 
 // router.get('/stylesheets', (req, res) => {
