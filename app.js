ad6882d2084540641dc140ba77765b7a5c032eb5
Isaias Pomales <impomales@gmail.com>
2018-04-19 11:41:14 -0400

    nunjucks config

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+'use strict';
+
 const express = require('express');
 const app = express();
+const nunjucks = require('nunjucks');
+
+const obj = {
+    people: [
+        { name: 'isaias'},
+        { name: 'Thanh'},
+        { name: 'Full' },
+        { name: 'Stack'}
+    ]
+}
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});
+
+const res = nunjucks.render('index.html', function(err, output) {
+    if (err) throw err;
+    console.log(output);
+})
 
 app.use((req, res, next)=> {
     let route = req.path
     let verb = req.method
     console.log(verb, route)
-    console.log(res.send(statusCode))
     next()
 })
-app.get('/',(req, res, next)=>  res.send('Welcome!'))
+app.get('/',(req, res, next)=>  res.render('index', obj))
 app.get('/news',(req, res, next)=>  res.send('News!'))
 
 
